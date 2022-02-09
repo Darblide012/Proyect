a36333c8e18739a847464ec9bcdcc56e8d5aad98
Darblide012 <68399627+Darblide012@users.noreply.github.com>
2022-02-09 16:39:54 -0600

    Implementacion de BDC

---
+++ promad/promad/Data/Model/Products.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace promad
+{
+    public class Products
+    {
+
+        [Key ]
+        public Guid IdProduct { get; set; }
+
+        [ForeignKey ("")]
+        public Guid IdProvider { get; set; }
+
+        [Column("")]
+        public string Code { get; set; }
+
+        [Column("")]
+        public string  Description { get; set; }
+
+        [Column("")]
+        public  string Unit { get; set; }
+
+        [Column("")]
+        public string Cost { get; set; }
+    }
+}
---
+++ promad/promad/Data/Model/Proveers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace promad
+{
+    public class Proveers
+    {
+
+        [Key]
+        public string Supplier { get; set; }
+
+        public Guid Code { get; set; }
+
+        public string BusinessName { get; set; }
+
+        public string RFC { get; set; }
+
+    }
+}
